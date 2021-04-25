73648117032eb22791fd1cd7c2420c245e587055
mdinicola <the.m.dinicola@gmail.com>
2021-04-25 15:49:56 -0400

    init 4.25.2021

--- Lab6.0_GC/Program.cs
+++ Lab6.0_GC/Program.cs
@@ -10,6 +10,7 @@ namespace Lab6._0_GC
             int faces = PosIntPrompt("How many faces are on your pair of dice? Positive integer: ");
             while (BoolPrompt($"Would you like to roll your {faces}-sided dice? (y/n)"))
             {
+                Console.WriteLine("");
                 RollDice(faces);
             }
         }
@@ -31,7 +32,6 @@ namespace Lab6._0_GC
         {
             while (true)
             {
-                bool returnVal = false;
                 Console.Write(message);
                 string input = Console.ReadLine().ToLower();
                 if (input == "yes" || input == "y")
@@ -46,9 +46,12 @@ namespace Lab6._0_GC
             }
         }
 
+        //initializes and plays a dice roller app
         public static void RollDice(int faces)
         {
-            //will instantiate a new DiceRollerApp object, and control the dice game
+            DiceRollerApp diceRoller = new DiceRollerApp(2, faces);
+            diceRoller.Roll();
+            diceRoller.Bonus();
         }
     }
 
@@ -57,48 +60,94 @@ namespace Lab6._0_GC
     {
         List<Die> dice;
 
-        // constructor defaults to 1 6-sided die
-        public DiceRollerApp()
+        // n m-sided dice
+        public DiceRollerApp(int diceNum, int faces)
         {
+            dice = new List<Die>();
+            for(int i=0; i<diceNum; i++)
+            {
+                dice.Add(new Die(faces));
+            }
 
         }
+
+        // constructor defaults to 1 6-sided die
+        public DiceRollerApp() : this(1, 6) { }
 
         // n 6-sided dice
-        public DiceRollerApp(int dice)
+        public DiceRollerApp(int diceNum) : this(diceNum, 6) { }
+
+        // prints and intro, rolls the dice, and leaves a space.
+        public void Roll()
+        {
+            int total = 0;
+            Console.WriteLine($"Your {((dice.Count==1)?"die":"dice")} rolled the following:");
+            for(int i=0; i< dice.Count; i++)
+            {
+                total+= dice[i].Roll();
+            }
+            Console.WriteLine("");
+        }
+
+        public void Read()
         {
 
         }
 
-        // n m-sided dice
-        public DiceRollerApp(int dice, int faces)
+        //allows special messages to be sent for the three special cases of craps, though it's unclear
+        //what should count as "boxcars" for dice with more/less than 6 faces.
+        public void Bonus()
         {
+            switch (Total())
+            {
+                case 2:
+                    Console.WriteLine("Craps! Snake Eyes!");
+                    break;
+                case 3:
+                    Console.WriteLine("Craps!");
+                    break;
+            }
+            if (Total() == (dice.Count * dice[0].Faces))
+            {
+                Console.WriteLine("Craps! Box cars!");
+            }
+        }
 
+        public int Total()
+        {
+            int total = 0;
+            for (int i = 0; i < dice.Count; i++)
+            {
+                total += dice[i].Value;
+            }
+            return total;
         }
     }
 
     // creates a Die object type, that the DiceRollerApp can roll
     public class Die
     {
+
+        System.Random random = new Random();
+
         // defaults to 6-sided. Each die gets it's own randomizer object
-        public Die()
-        {
-            Faces = 6;
-            System.Random random = new Random();
-        }
+        public Die() : this(6) { }
 
         public Die(int faces)
         {
             Faces = faces;
-            System.Random random = new Random();
+            Value = 0;      //Value is 0 until die is rolled
         }
 
         public int Faces { get; set; }
-        public Random random { get; }
+        public int Value { get; set; }
 
         // the Roll method can be called to grab the next random number for a die
         public int Roll()
         {
-            return random.Next(Faces) + 1;
+            Value = random.Next(1, Faces+1);
+            Console.WriteLine(Value);
+            return Value;
         }
     }
     // future extention description:
